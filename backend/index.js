03178bd53abbdfaebf832d5ce51b4618589dcf3d
Debojyoti <sahadebojyoti955@gmail.com>
2025-04-19 21:55:18 +0530

    sixth Commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,12 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 1000
 
-app.use(cors({origin: "http://localhost:3000", credentials: true}))
+// app.use(cors({origin: "http://localhost:3000 ", credentials: true}))
+app.use(cors({
+  origin: 'https://recipe-recomendation-system-frontendsite.onrender.com',  // Frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,  // If using cookies/sessions
+}));
 app.use(express.json())
 app.use(cookieParser())
 
